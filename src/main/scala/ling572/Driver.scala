025b2d3b892f4f78963aa883b83a47ce2289bad1
chris <cmc3c@uw.edu>
2013-02-22 21:47:44 -0500

    scala Driver

--- src/main/scala/ling572/Driver.scala
+++ src/main/scala/ling572/Driver.scala
@@ -1,48 +1,96 @@
 package ling572
 
-import ling572.util.VectorFileReader
+import util.{Instance, VectorFileReader}
 import java.io.File
-
-import scala.collection.JavaConverters._
+import scala.collection._
 
 object Driver extends App {
 
-  var trainingData:File = null
+  var testData:File = null
+  var boundaryData:File = null
   var outputFile:File = null
   var modelFile:File = null
+  var beamSize:Double  = 0.0
+  var topN:Int = 0
+  var topK:Int = 0
+
 
   def exit(errorText:String) {
     System.out.println(errorText)
     System.exit(1)
   }
 
-  if (args.length < 2 || args.length > 3)
-   exit("Error: usage Driver training_data output_file [model_file]")
+  if (args.length != 7)
+   exit("Error: usage Driver test_data boundary_data model_file output_file beam_size top_N top_K")
 
   try {
-    this.trainingData = new File(args(0))
+    this.testData = new File(args(0))
   } catch {
-    case e:Exception => exit("Error: invalid training_data file")
+    case e:Exception => exit("Error: invalid test_data file")
   }
 
   try {
-    this.outputFile = new File(args(1))
+    this.boundaryData = new File(args(1))
+  } catch {
+    case e:Exception => exit("Error: invalid boundary_data file")
+  }
+
+  try {
+    this.modelFile = new File(args(2))
+  } catch {
+    case e:Exception => exit("Error: invalid model file")
+  }
+
+  try {
+    this.outputFile = new File(args(3))
   } catch {
     case e:Exception => exit("Error: invalid output_file")
   }
 
-  if (args.length == 3) {
-    try {
-      this.modelFile = new File(args(2))
-    } catch {
-      case e:Exception => exit("Error: invalid model_file")
-    }
-  } else {
-    this.modelFile = null
+
+  try {
+    this.beamSize = args(4).toDouble
+  } catch {
+      case e:Exception => exit("Error: invalid beam_size")
   }
 
-  val instances = VectorFileReader.indexInstances(trainingData).asScala.toList
+  try {
+    this.topK = args(5).toInt
+  } catch {
+    case e:Exception => exit("Error: invalid top_N")
+  }
 
+  try {
+    this.topN = args(6).toInt
+  } catch {
+    case e:Exception => exit("Error: invalid top_K")
+  }
+
+  println("args(4): [" + args(4) + "]  beam_width: " + beamSize)
+  println("args(5): [" + args(5) + "]  topK: " + topK)
+  println("args(6): [" + args(6) + "]  topN: " + topN)
+
+
+  val model = new MaxEntModel()
+  model.loadFromFile(modelFile)
+
+  val allInstances: java.util.List[Instance] = VectorFileReader.indexInstances(testData)
+
+  val beamSearch = new BeamSearch(topK, topN, beamSize, model)
+  beamSearch.search(allInstances)
+
+  var node = beamSearch.getBestNode
+
+  val tags = new mutable.ArrayBuffer[String] // mapResult { xs => mutable.LinkedList( xs:_* ) }
+
+  while (node.getParent != null) {
+      tags += (node.getName + " " + node.getGoldTag + " " + node.getTag + " " + node.getNodeProb)
+    node = node.getParent
+  }
+
+  for (tag <- tags.reverse) {
+    System.out.println(tag)
+  }
 
 }
 
